3443effb9532c31e4163db1e34616bb7288d56bb
Ak Razu <fbook3603@gmail.com>
2023-01-16 15:28:56 +0600

    footer add

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from '../../../assets/logo.svg'
+const Footer = () => {
+  return (
+    <footer className="footer p-32 bg-black text-white">
+      <div>
+        <img src={logo} alt="" />
+        <p>
+        Edwin Diaz is a software and web <br /> technologies engineer, a life coach <br /> trainer who is also a serial .
+        </p>
+      </div>
+      <div>
+        <span className="footer-title">About</span>
+        <a href='/' className="link link-hover">Home</a>
+        <a href='/' className="link link-hover">Service</a>
+        <a href='/' className="link link-hover">Contact</a>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <a href='/' className="link link-hover">Why Car Doctor</a>
+        <a href='/' className="link link-hover">About</a>
+      </div>
+      <div>
+        <span className="footer-title">Support</span>
+        <a href='/' className="link link-hover">Support Center</a>
+        <a href='/' className="link link-hover">Feedback</a>
+        <a href='/' className="link link-hover">Accessability</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
