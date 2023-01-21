676a776e218de8d42275c9a625d55df437282020
Ak Razu <fbook3603@gmail.com>
2023-01-21 20:02:47 +0600

    service section more add

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,17 +1,19 @@
 import React from "react";
-
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 const ServiceCard = ({ service }) => {
-    const {img,title,price}=service;
+  const { img, title, price } = service;
   return (
-    <div className="card card-compact bg-base-100 shadow-xl">
-      <figure>
-        <img src={img} alt="Shoes" />
-      </figure>
+    <div className="card card-compact border p-8 rounded-lg  hover:shadow-xl cursor-pointer bg-base-100">
+      <div>
+        <img className=" rounded-lg" src={img} alt="" />
+      </div>
       <div className="card-body">
         <h2 className="text-[#444444] font-bold text-2xl">{title}</h2>
-        <p className="text-2xl text-orange-600 font-semibold">Price : ${price}</p>
-        <div className="card-actions justify-end">
-          <button className="btn border-0 btn-circle bg-orange-600"> &gt; </button>
+        <div className="flex justify-between items-center">
+          <p className="text-2xl text-orange-600 font-semibold">
+            Price : ${price}
+          </p>
+          <ArrowRightIcon className="h-5 w-5 cursor-pointer  text-[#FF912C]" />
         </div>
       </div>
     </div>
